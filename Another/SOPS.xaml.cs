424b56903367b3f8e86d6dccd19e6fca01c9f11c
toshmatov1991 <103163285+toshmatov1991@users.noreply.github.com>
2022-12-24 13:08:28 +0700

    Rjhhtrn

--- Another/SOPS.xaml.cs
+++ Another/SOPS.xaml.cs
@@ -1,17 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace CP.Another
 {
@@ -47,8 +35,5 @@ namespace CP.Another
                 }
             }
         }
-
-
-
     }
 }

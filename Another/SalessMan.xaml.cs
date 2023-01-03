0e30c9dbad3840efd72bab2e5576e44756e5f4d1
toshmatov1991 <103163285+toshmatov1991@users.noreply.github.com>
2023-01-03 16:04:02 +0700

    123123

--- Another/SalessMan.xaml.cs
+++ Another/SalessMan.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,14 +15,41 @@ using System.Windows.Shapes;
 
 namespace CP.Another
 {
-    /// <summary>
-    /// Логика взаимодействия для SalessMan.xaml
-    /// </summary>
     public partial class SalessMan : Window
     {
         public SalessMan()
         {
             InitializeComponent();
+            GetMyClients();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Добавить в базу клиента
+
+        }
+
+        private async void GetMyClients()
+        {
+            using (RealContext db = new())
+            {
+                var getget = from clie in await db.Clients.ToListAsync()
+                             join pass in await db.Passports.ToListAsync() on clie.PasswordFk equals pass.Id
+                             select new
+                             {
+                                 clie.Id,
+                                 f = clie.Firstname,
+                                 n = clie.Name,
+                                 l = clie.Lastname,
+                                 nf = clie.Numberphone,
+                                 s = pass.Serial,
+                                 number = pass.Number,
+                                 dv = pass.Dateof,
+                                 kv = pass.Isby
+                             };
+                listviewCards.ItemsSource = getget.ToList();
+            }
+        }
+
     }
 }

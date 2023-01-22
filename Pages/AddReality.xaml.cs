4115253645a7ad0f743994ad3d1af9490167efc9
toshmatov1991 <103163285+toshmatov1991@users.noreply.github.com>
2023-01-22 20:24:16 +0700

    логика 1

--- Pages/AddReality.xaml.cs
+++ Pages/AddReality.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,8 +22,39 @@ namespace CP.Pages
         public AddReality()
         {
             InitializeComponent();
+            new Thread(UpDateData).Start();
         }
 
+
+        //Заполнить Comboboxes данными из связанных таблиц
+        private void UpDateData()
+        {
+            using(RealContext db = new())
+            {
+                //Типы объектов
+                var typeObj = db.ObjectTypes.FromSqlRaw("SELECT * FROM ObjectType").ToList();
+                List<string> list = new();
+                foreach (var type in typeObj){list.Add(type.Name);}
+
+                //Районы
+                var distr = db.Districts.FromSqlRaw("SELECT * FROM Districts").ToList();
+                List<string> list1 = new();
+                foreach (var item in distr) {list1.Add(item.Name);}
+               
+                //Санузел
+                var sunuz = db.BathroomTypes.FromSqlRaw("SELECT * FROM BathroomType").ToList();
+                List<string> list2 = new();
+                foreach (var item in sunuz) { list2.Add(item.Type); }
+                
+
+                Dispatcher.Invoke(() =>
+                {
+                    category.ItemsSource = list;
+                    rayon.ItemsSource = list1;
+                    sunuzel.ItemsSource = list2;
+                });
+            }
+        }
 
 
 

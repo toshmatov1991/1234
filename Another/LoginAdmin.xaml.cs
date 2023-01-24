4d36f4829b0bee276d5e38cc18fe8f8ce9b156db
toshmatov1991 <103163285+toshmatov1991@users.noreply.github.com>
2023-01-24 21:18:58 +0700

    12121

---
+++ Another/LoginAdmin.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CP.Another
+{
+    public partial class LoginAdmin : Window
+    {
+        public LoginAdmin()
+        {
+            InitializeComponent();
+            passe.Focus();
+        }
+
+        private void GoAdm(object sender, KeyEventArgs e)
+        {
+            if (passe.Password == "qwerty123")
+            {
+                User.clo = 1;
+                Close();
+            }
+        }
+
+        private void df_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
--- User.xaml.cs
+++ User.xaml.cs
@@ -1,9 +1,11 @@
-﻿using CP.Models;
+﻿using CP.Another;
+using CP.Models;
 using CP.Pages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,32 +20,59 @@ namespace CP
 {
     public partial class User : Window
     {
+        public static int clo = 0;
+        public static bool fer = true;
         int qwe = 0;
         public User(string str, int us)
         {
             InitializeComponent();
             qwe = us;
             Title = str;
-            //MainFrame.Content = new Pages.Prodazha(qwe);
-            MainFrame.Content = new Pages.Arhiv();
+            MainFrame.Content = new Pages.Prodazha(qwe);
         }
 
         private void LeFt(object sender, MouseButtonEventArgs e)
         {
-            MainFrame.Content = new Pages.Prodazha(qwe);
+            MainFrame.Content = new Prodazha(qwe);
             AddReality.potok = false;
         }
 
         private void AdDRealse(object sender, MouseButtonEventArgs e)
         {
-            MainFrame.Content = new Pages.AddReality();
+            MainFrame.Content = new AddReality();
             AddReality.potok = true;
         }
 
         private void SeeArhive(object sender, MouseButtonEventArgs e)
         {
             AddReality.potok = false;
-            MainFrame.Content = new Pages.Arhiv();
+            MainFrame.Content = new Arhiv();
         }
+
+        private void NuLadno(object sender, MouseButtonEventArgs e)
+        {
+            MainFrame.Content = new Page1();
+            LoginAdmin login = new();
+            login.ShowDialog();
+            AddReality.potok = false;
+            new Thread(UPUP).Start();
+            
+        }
+
+        private void UPUP()
+        {
+            while (true)
+            {
+                if (clo == 1)
+                    Dispatcher.Invoke(() =>
+                    {
+                        MainFrame.Content = new Addmin();
+                    });
+                    
+            }
+        }
+
+
+
     }
 }
